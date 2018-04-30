2471e610d1e7f7c8283c6f6e8706344c6d41616c
Sachith Ushan <sachith@calcey.com>
2018-04-30 17:28:59 +0530

    Adding login

---
+++ MarkPredictor/MarkPredictor/Controllers/Account/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MarkPredictor.Dto;
+using MarkPredictor.Common;
+
+namespace MarkPredictor.Controllers.Account
+{
+    public class AccountController : IAccountController
+    {
+        private HttpClient _httpClient;
+
+        public AccountController()
+        {
+            _httpClient = InstanceFactory.GetHttpClientInstance();
+        }
+
+        public async Task<StudentDto> login(StudentDto studentDto)
+        {
+            return await _httpClient.Login(studentDto);
+        }
+    }
+}
